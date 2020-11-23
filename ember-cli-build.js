a12116b3d768f0fc55e8d81c14c2e79b1c87c1a9
Laura Mun <laura@peeriq.com>
2020-11-23 16:11:59 -0500

    Fix for Mass upgrade dependencies #177

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,7 +8,8 @@ module.exports = function(defaults) {
     autoImport: {
       webpack: {
         node: {
-          global: true
+          global: true,
+          fs: 'empty'
         }
       }
     }

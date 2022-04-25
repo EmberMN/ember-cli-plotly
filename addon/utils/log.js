3e0e6866036b8f139b5e59a5ac0804fc4564805c
Jacob Quant <jacobq@gmail.com>
2022-04-25 15:41:40 -0500

    refactor: do not load debug dynamically

--- addon/utils/log.js
+++ addon/utils/log.js
@@ -1,20 +1,10 @@
+import debug from 'debug';
+
 export function getLoggingFunctions(logNamespace) {
-  let log = console.log.bind(console);
-  let logVerbose = console.log.bind(console);
-  let warn = console.warn.bind(console);
-  import('debug')
-    .then((d) => {
-      const debug = d.default;
-      log = debug(logNamespace);
-      logVerbose = debug(`${logNamespace}:verbose`);
-      warn = debug(logNamespace);
-      warn.log = console.warn.bind(console); // eslint-disable-line no-console
-    })
-    .catch(() => {
-      // if debug isn't present, suppress all except warnings
-      log = (...args) => undefined; // eslint-disable-line no-unused-vars
-      logVerbose = log;
-    });
+  const log = debug(logNamespace);
+  const logVerbose = debug(`${logNamespace}:verbose`);
+  const warn = debug(logNamespace);
+  warn.log = console.warn.bind(console); // eslint-disable-line no-console
 
   return {
     log,
